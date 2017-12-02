3ade60ec49b8ad26d849ef510af5f683b9d73b6e
Samuel <s.hoelzl@gmx.at>
2017-12-02 16:09:49 +0100

    added actors classes and akka nuget

---
+++ Common/Actors/TestRunCoordinator.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+using Common.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAgent.Actors
+{
+    public class TestRunCoordinator : ReceiveActor
+    {
+        
+
+        public TestRunCoordinator()
+        {
+            Console.WriteLine($"TestRunCoordinator created");
+            Receive<StartNewLoadTest>(message => StartNewLoadTest(message));
+        }
+
+        private void StartNewLoadTest(StartNewLoadTest message)
+        {
+            
+        }
+
+    }
+}
--- TestAgent/Program.cs
+++ TestAgent/Program.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using Akka.Actor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using TestAgent.Actors;
 
 namespace TestAgent {
     class Program {
         static void Main(string[] args) {
-            for (;;) {
-                Console.WriteLine("Hello World!");
-                Thread.Sleep(1000);
+            Console.WriteLine("Test Agent");
+            using (var system = ActorSystem.Create("TestAgent"))
+            {
+                //TODO remove (only for testing)
+                system.ActorOf(Props.Create<TestRunCoordinator>(), "test run coordinator");
+
+                system.WhenTerminated.Wait();
             }
         }
     }
