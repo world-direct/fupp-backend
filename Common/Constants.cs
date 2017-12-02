4e2f1f544e66d7d9f7a4a27048629aab58e650a8
Samuel <s.hoelzl@gmx.at>
2017-12-02 15:35:08 +0100

    added Common project

---
+++ Common/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Constants
+    {
+
+        public class Topics
+        {
+            public readonly string AGENT_FINISHED = "AGENT_FINISHED";
+            public readonly string TEST_RESULT_ANALYSED = "TEST_RESULT_ANALYSED";
+            public readonly string TEST_STARTED = "TEST_STARTED";
+        }
+
+    }
+}
