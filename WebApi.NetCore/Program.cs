99e47782662fba12d26b0088514ec1247ccf10a0
Christoph Aichinger <aichinger-chr@gmx.at>
2018-01-05 13:40:53 +0100

    fix akka pubsub distribution config

--- WebApi.NetCore/Program.cs
+++ WebApi.NetCore/Program.cs
@@ -7,14 +7,20 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Common.Utility;
+using Akka.Actor;
 
 namespace WebApi.NetCore
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+    {
+            using (var actorSystem = ActorSystemProvider.ActorSystem) {
+                actorSystem.ActorOf(Props.Create<StartTestRequestActor>(), "StartTestRequest");
+                actorSystem.ActorOf(Props.Create<TestResultsRequestActor>(), "TestResultsRequest");
+                BuildWebHost(args).Run();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
